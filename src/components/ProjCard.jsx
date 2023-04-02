efc26d3080ca74c529a311ba95727a2af57b588a
Gabbie Bade <gabbiebade@gmail.com>
2023-04-02 16:48:15 -0700

    base hover for projcard

--- src/components/ProjCard.jsx
+++ src/components/ProjCard.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../sass/styles.scss";
 
 const ProjCard = ({ image, title, tools, description, pathname }) => {
   return (
     <article className="project-card">
       <Link to={pathname} className="info-link">
         <img src={image} alt="works" />
-
-        <div className="details-container">
           <h2>{title}</h2>
-          {tools ? (
-            <ul className="tools">
-              {tools.map((tool, index) => (
-                <li key={index}>{tool}</li>
-              ))}
-            </ul>
-          ) : null}
-
-          <p className="projext-description">{description}</p>
-        </div>
+          <p id="read-more-cta">Click to read more</p>
       </Link>
+
     </article>
   );
 };
